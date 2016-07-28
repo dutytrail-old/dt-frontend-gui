2215a840ae425ffab29c13d0905b5c90e64c1f10
joan.tolos <joan.tolos@thomsonreuters.com>
2016-07-28 17:29:17 +0200

    Simple duty page

--- src/main/webapp/resources/javascript/functionality/duty.js
+++ src/main/webapp/resources/javascript/functionality/duty.js
@@ -6,7 +6,19 @@ function getDutyList(userId) {
             success : function(data, textStatus, jqXHR) {
                 if (data.length > 0) {
                     for (var i = 0; i < data.length; i++) {
-                        $("#dutyList").append('<div class="duty-item"><div class="text-info"><li>' + data[i].name + '</li></div></div>');
+                        $("#dutyList").append(
+                        '<li class="mt-list-item done">'+
+                        '    <div class="list-icon-container">'+
+                        '        <i class="icon-check"></i>'+
+                        '    </div>'+
+                        '    <div class="list-datetime"> 8 Nov </div>'+
+                        '    <div class="list-item-content">'+
+                        '        <h3 class="uppercase">'+
+                        '            <a href="javascript:;">' + data[i].name + '</a>'+
+                        '        </h3>'+
+                        '    </div>'+
+                        '</li>'
+                        );
                     }
                 }
             },
